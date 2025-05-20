dc7c6a67a7718bc3f73b0c03c1786bcdf3717c6b
rajeshmurmu <rajeshsonmurmur2017@gmail.com>
2025-05-20 12:56:51 +0530

    remove theme

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router";
 import App from "./App";
-import { ThemeProvider } from "./hooks/theme-provider";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <App />
   </BrowserRouter>
 );
